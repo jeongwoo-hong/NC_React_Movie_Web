008a8b63722750a7b10d3d8cfa8d7af7fc340562
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-12 23:54:25 +0900

    22.12.12.8

--- react-for-beginner/src/App2.js
+++ react-for-beginner/src/App2.js
@@ -4,12 +4,17 @@ import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+      setKeyword(event.target.value);
+  }
   console.log("I run all the time")
   useEffect(() => {
       console.log("Call the API...")
   }, []);
   return (
     <div>
+        <input value={keyword} type="text" placeholder="Search Here" onChange={onChange}/>
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
