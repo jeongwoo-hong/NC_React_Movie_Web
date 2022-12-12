cb47dedbf28a4382c08450c44e3aec2a384a8e70
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-12 23:44:55 +0900

    22.12.12.5

---
+++ react-for-beginner/src/App2.js
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  return (
+    <div>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
