8fe11a05342ce81c2e1eabe88b0c8e4b96ceb9eb
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-12 23:45:39 +0900

    22.12.12.6

--- react-for-beginner/src/App2.js
+++ react-for-beginner/src/App2.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api")
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
