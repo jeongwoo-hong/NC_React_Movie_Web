ab714743661ec4d1aa73fa57149477278248538e
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-15 23:42:01 +0900

    22.12.15.7

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -6,7 +6,12 @@ function MovieApp() {
     useEffect(() => {
         fetch(
             `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-        ).then((response) => response.json()).then(json => setMovies(json.data.movies))
+        )
+        .then((response) => response.json())
+        .then(json => {
+            setMovies(json.data.movies);
+            setLoading(false);
+        })
     }, [])
    console.log(movies)
     return <div>
