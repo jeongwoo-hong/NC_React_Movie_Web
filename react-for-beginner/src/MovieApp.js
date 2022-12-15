2d087ff1ad5dc4556f70c11fd7af47462d2b3f47
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-15 23:40:57 +0900

    22.12.15.6

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 
 function MovieApp() {
     const [loading, setLoading] = useState(true);
-    useEffect(() => {}, [])
-    fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    ).then((response) => response.json()).then(json => console.log(json))
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+        ).then((response) => response.json()).then(json => setMovies(json.data.movies))
+    }, [])
+   console.log(movies)
     return <div>
         {loading ? <h1>Loading...</h1> : null}
     </div>
