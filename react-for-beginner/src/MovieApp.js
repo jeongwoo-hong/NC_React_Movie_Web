6fe20e35c066e8099ce92dedba1c7ed8c77159c0
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-15 23:36:52 +0900

    22.12.15.2

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function MovieApp() {
     const [loading, setLoading] = useState(true);
-
+    useEffect(() => {}, [])
     return <div>
         {loading ? <h1>Loading...</h1> : null}
     </div>
