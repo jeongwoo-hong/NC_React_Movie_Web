a536a5f2f5eb1761a23ae73dba5b390de55ef010
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-16 23:41:56 +0900

    22.12.16.5

--- react-for-beginner/src/Movie.js
+++ react-for-beginner/src/Movie.js
@@ -1,5 +1,13 @@
 function Movie(){
-    return null;
+    return <div>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2><h3>({movie.year})</h3>
+            <h4>rating : {movie.rating}</h4>
+            <p>{movie.summary}</p>
+            <ul>
+                {movie.geners.map((g) => <li key={g}>{g}</li>)}
+            </ul>
+    </div>;
 }
 
 export default Movie;
--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -18,13 +18,7 @@ function MovieApp() {
     return <div>
         {loading ? <h1>Loading...</h1> : movies.map(movie => (
         <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2><h3>({movie.year})</h3>
-            <h4>rating : {movie.rating}</h4>
-            <p>{movie.summary}</p>
-            <ul>
-                {movie.geners.map((g) => <li key={g}>{g}</li>)}
-            </ul>
+            
             </div>
         ))}
     </div>
