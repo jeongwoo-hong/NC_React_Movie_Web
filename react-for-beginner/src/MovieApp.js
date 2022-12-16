629573dbc5a92258e0d9b5bf4365ce74f3e1b9f4
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-16 21:58:25 +0900

    22.12.16.2

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -19,7 +19,7 @@ function MovieApp() {
         {loading ? <h1>Loading...</h1> : movies.map(movie => (
         <div key={movie.id}>
             <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
+            <h2>{movie.title}</h2><h3>({movie.year})</h3>
             <p>{movie.summary}</p>
             <ul>
                 {movie.geners.map((g) => <li key={g}>{g}</li>)}
