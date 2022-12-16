d8897275ff8505dde248f0023c8f7aab9bd1f631
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-16 23:43:05 +0900

    22.12.16.6

--- react-for-beginner/src/Movie.js
+++ react-for-beginner/src/Movie.js
@@ -1,11 +1,10 @@
-function Movie(){
+function Movie({medium_cover_image, title, summary, genres}){
     return <div>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2><h3>({movie.year})</h3>
-            <h4>rating : {movie.rating}</h4>
-            <p>{movie.summary}</p>
+            <img src={medium_cover_image} />
+            <h2>{title}</h2>
+            <p>{summary}</p>
             <ul>
-                {movie.geners.map((g) => <li key={g}>{g}</li>)}
+                {genres.map((g) => <li key={g}>{g}</li>)}
             </ul>
     </div>;
 }
