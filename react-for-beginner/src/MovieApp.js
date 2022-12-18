e213de56d1811f655df31556146b04fc538e52ed
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-18 21:36:09 +0900

    22.12.18.9

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -3,30 +3,7 @@ import { useEffect, useState } from "react";
 import Movie from "./components/Movie"
 
 function MovieApp() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([])
-    const getMovies = async() => {
-        const response = await fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-        )
-        const json = await response.json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies()
-    }, []);
-    return <div>
-        {loading ? <h1>Loading...</h1> : movies.map(movie => (
-        <Movie 
-        key={movie.id}
-        coverImg={movie.medium_cover_image} 
-        title={movie.title} 
-        summary={movie.summary} 
-        genres={movie.genres} 
-        />
-        ))}
-    </div>
+    return null;
 }
 
 export default MovieApp;
