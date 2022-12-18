1a17b05a034a44f937669a163653617f23476957
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-19 08:13:40 +0900

    22.12.19.2

--- react-for-beginner/src/MovieApp.js
+++ react-for-beginner/src/MovieApp.js
@@ -1,9 +1,22 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie"
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home"
 
 function MovieApp() {
-    return null;
+    return <Router>
+        <Switch>
+            <Route path="/movie">
+                <Detail/>
+            </Route>
+            <Route path="/">
+                <Home/>
+            </Route>
+        </Switch>
+    </Router>;
 }
 
 export default MovieApp;
